456b230eb8c49f7d9c122d062bf8b8d7c7f273de
Sandro Reis <sandrouh@hotmail.com>
2020-08-02 19:04:55 -0300

    feat: add the loading icon

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,29 +1,60 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import Loader from '../Components/Loader'
 
 export default function Product () {
   const { id } = useParams()
-  const [product, setProduct] = useState(null)
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+    error: false
+  })
   const url = `https://5f26dc810824d8001655ec9d.mockapi.io/products/${id}`
 
   let content = null
 
   useEffect(() => {
-    axios.get(url).then(response => {
-      setProduct(response.data)
+    setProduct({
+      loading: true,
+      data: null,
+      error: false
     })
+    axios
+      .get(url)
+      .then(response => {
+        setProduct({
+          loading: false,
+          data: response.data,
+          error: false
+        })
+      })
+      .catch(() => {
+        setProduct({
+          loading: false,
+          data: null,
+          error: true
+        })
+      })
   }, [url])
 
-  if (product) {
+  if (product.error) {
+    content = <p>There was an error refresh or try again</p>
+  }
+
+  if (product.loading) {
+    content = <Loader />
+  }
+
+  if (product.data) {
     content = (
       <div>
-        <h1 className='text-2xl font-bold mb-3'>{product.name}</h1>
+        <h1 className='text-2xl font-bold mb-3'>{product.data.name}</h1>
         <div>
-          <img src={product.images[0].imageUrl} alt={product.name} />
+          <img src={product.data.images[0].imageUrl} alt={product.data.name} />
         </div>
-        <div className='font-bold text-xl mb-3'>$ {product.price}</div>
-        <div>{product.description}</div>
+        <div className='font-bold text-xl mb-3'>$ {product.data.price}</div>
+        <div>{product.data.description}</div>
       </div>
     )
   }
