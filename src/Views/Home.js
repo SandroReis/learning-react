b658d091d22132f4b36b71ae5b9de458eb14a0f9
Sandro Reis <sandrouh@hotmail.com>
2020-08-02 19:46:07 -0300

    refactor: add a hook for the http requests

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,41 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 import Loader from '../Components/Loader'
 import ProductCard from '../Components/ProductCard'
+import useAxiosGet from '../Hooks/HttpRequests'
 
 export default function Home () {
   let content = null
   const url =
     'https://5f26dc810824d8001655ec9d.mockapi.io/products?page=1&limit=10'
-  const [products, setProducts] = useState({
-    loading: false,
-    data: null,
-    error: false
-  })
 
-  useEffect(() => {
-    setProducts({
-      loading: true,
-      data: null,
-      error: false
-    })
-    axios
-      .get(url)
-      .then(response => {
-        setProducts({
-          loading: false,
-          data: response.data,
-          error: false
-        })
-      })
-      .catch(() => {
-        setProducts({
-          loading: false,
-          data: null,
-          error: true
-        })
-      })
-  }, [url])
+  const products = useAxiosGet(url)
 
   if (products.error) {
     content = <p>There was an error refresh or try again</p>
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,42 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 import Loader from '../Components/Loader'
+import useAxiosGet from '../Hooks/HttpRequests'
 
 export default function Product () {
   const { id } = useParams()
   const url = `https://5f26dc810824d8001655ec9d.mockapi.io/products/${id}`
-  const [product, setProduct] = useState({
-    loading: false,
-    data: null,
-    error: false
-  })
+
+  const product = useAxiosGet(url)
 
   let content = null
-
-  useEffect(() => {
-    setProduct({
-      loading: true,
-      data: null,
-      error: false
-    })
-    axios
-      .get(url)
-      .then(response => {
-        setProduct({
-          loading: false,
-          data: response.data,
-          error: false
-        })
-      })
-      .catch(() => {
-        setProduct({
-          loading: false,
-          data: null,
-          error: true
-        })
-      })
-  }, [url])
 
   if (product.error) {
     content = <p>There was an error refresh or try again</p>
