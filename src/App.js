9250edca2dcb2987f867969f82fc238989f6fb59
Sandro Reis <sandrouh@hotmail.com>
2020-08-02 11:34:22 -0300

    feat: add some components, class 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import './App.css'
+import Header from './Components/Header'
 import HelloWorld from './Components/HelloWorld'
+import Footer from './Components/Footer'
 
 function App () {
   return (
     <div className=''>
+      <Header />
       <HelloWorld name='Sandro' />
+      <Footer />
     </div>
   )
 }
--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default class HelloWorld extends React.Component {
   render () {
-    return <h1>Hello {this.props.name}</h1>
+    return <h2>Hello {this.props.name}</h2>
   }
 }
